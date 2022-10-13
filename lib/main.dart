e7d9e6f62b60211876a90fb81948d2c98cbf4ae1
chloewong108 <chloewng@ymail.com>
2022-10-13 12:05:48 +0100

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,29 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import './navBar.dart';
 import './drinks.dart';
 import './home.dart';
 import './profile.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(),
-      title: 'Flutter Demo',
+      title: 'Coffee, Help!',
       home: App(),
     );
   }
